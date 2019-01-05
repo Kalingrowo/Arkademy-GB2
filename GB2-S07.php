3255f4f075f55c470831f293bb63e6ddadbff783
Rizqon <m.rum.rowi.r@gmail.com>
2019-01-05 20:11:24 +0700

    Clean unused file

--- GB2-S07.php
+++ GB2-S07.php
@@ -58,17 +58,18 @@
     </div>
   </div>
 
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2018 Copyright:
-    <a href="https://github.com/Kalingrowo/"> Kalingrowo</a>
-  </div>
-  <!-- Copyright -->
-
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </body>
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4 fixed-bottom">
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="https://github.com/Kalingrowo/"> Kalingrowo</a>
+  </div>
+  <!-- Copyright -->
+</footer>
 </html>
