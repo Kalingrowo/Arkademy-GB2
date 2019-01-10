03ed406dfc889e286f3d9d8b6d02744ba1ce9ad1
Rizqon <m.rum.rowi.r@gmail.com>
2019-01-10 19:48:23 +0700

    update after technical interview

--- GB2-S03.php
+++ GB2-S03.php
@@ -14,5 +14,20 @@
     }
   }
 
+  // function drawLine(int)
+  function drawLine2($length) {
+    for($i=$length; $i >= 0; $i--) {
+      for ($j=1; $j <= $i; $j++) {
+        if ($i == $j) {
+          echo "*";
+          echo "<br />";
+        } else {
+          echo "&nbsp;&nbsp;&nbsp;";
+        }
+      }
+    }
+  }
+
   drawLine(8);
+  drawLine2(8);
 ?>
